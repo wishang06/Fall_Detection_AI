5547bab547225324c1c37214c475c60b268f2f13
username <useremail>
2025-08-27 01:04:15 +1000

    put an assets of sound recording for test sound to text function

--- sound.py
+++ sound.py
@@ -3,7 +3,7 @@ from faster_whisper import WhisperModel
 # model sizes: tiny, base, small, medium, large-v3 (bigger = better but slower)
 model = WhisperModel("small", device="cpu")   # use device="cuda" if you have NVIDIA GPU
 
-segments, info = model.transcribe("example.wav", beam_size=5)
+segments, info = model.transcribe("assets/test.m4a", beam_size=5)
 print(f"Detected language: {info.language} (prob {info.language_probability:.2f})")
 text = "".join(seg.text for seg in segments)
 print(text)
